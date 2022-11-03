046e788d12240c318d6a6d093de37489b53fafc2
ScuffedItalian <anthony.locascio@outlook.com>
2022-11-02 19:50:37 -0700

    Bro... update again :o holy cow

--- src/EnvTest.ts
+++ src/EnvTest.ts
@@ -1,8 +1,10 @@
 import {Environment} from "https://deno.land/x/remapper@2.1.0/src/mod.ts" // MAKE SURE TO USE THE LATEST REMAPPER VERSION HERE
 
-export function envE(scale: number){
+export function envE(disabled: boolean){
     const enviTest = new Environment("Environment", "Contains")
-    enviTest.scale = [scale, scale, scale]
-    enviTest.push();
+    if(disabled == true){
+        enviTest.position = [0, -999999, 0]
+        enviTest.push();
+    }
 }
 
