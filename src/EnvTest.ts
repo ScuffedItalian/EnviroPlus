352ff7c67732db86e25dd2add24b94aeea9f9eef
ScuffedItalian <anthony.locascio@outlook.com>
2022-11-02 21:58:40 -0700

    Update again...

--- src/EnvTest.ts
+++ src/EnvTest.ts
@@ -1,10 +1,13 @@
-import {Environment} from "https://deno.land/x/remapper@2.1.0/src/mod.ts" // MAKE SURE TO USE THE LATEST REMAPPER VERSION HERE
+import {Environment} from "https://deno.land/x/remapper@2.1.0/src/mod.ts" 
 
-export function envE(disabled: boolean){
-    const enviTest = new Environment("Environment", "Contains")
-    if(disabled == true){
-        enviTest.position = [0, -999999, 0]
-        enviTest.push();
-    }
+
+//Environments
+
+
+
+export function envSetting(scale: number){
+    const envSet = new Environment("Environment", "Contains")
+    envSet.scale = [scale, scale, scale];
+    envSet.push();
+
 }
-
