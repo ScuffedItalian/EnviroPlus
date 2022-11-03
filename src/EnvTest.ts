acd61e758d6e4c340a7cbc606c55d358f7fb77cd
ScuffedItalian <anthony.locascio@outlook.com>
2022-11-02 22:10:03 -0700

    :o YAY

--- src/EnvTest.ts
+++ src/EnvTest.ts
@@ -1,13 +1,13 @@
-import {Environment} from "https://deno.land/x/remapper@2.1.0/src/mod.ts" 
+import {Environment, Vec3} from "https://deno.land/x/remapper@2.1.0/src/mod.ts" 
 
 
 //Environments
 
 
 
-export function envSetting(scale: number){
+export function envSetting(scale: Vec3){
     const envSet = new Environment("Environment", "Contains")
-    envSet.scale = [scale, scale, scale];
+    envSet.scale = scale
     envSet.push();
 
 }
