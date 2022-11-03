f8fbf39e6860c59af111f2fb1612eb48d1f1e88e
ScuffedItalian <anthony.locascio@outlook.com>
2022-11-02 19:29:33 -0700

    :o New commit?

--- src/EnvTest.ts
+++ src/EnvTest.ts
@@ -1,7 +1,8 @@
 import {Environment} from "https://deno.land/x/remapper@2.1.0/src/mod.ts" // MAKE SURE TO USE THE LATEST REMAPPER VERSION HERE
 
-export function envT(scale: number){
+export function envE(scale: number){
     const enviTest = new Environment("Environment", "Contains")
     enviTest.scale = [scale, scale, scale]
     enviTest.push();
 }
+
