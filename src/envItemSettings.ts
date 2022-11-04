680ae8757828dadce33a6d401e06113e21ba4667
ScuffedItalian <anthony.locascio@outlook.com>
2022-11-04 14:39:46 -0700

    ME DID IT :D

--- src/envItemSettings.ts
+++ src/envItemSettings.ts
@@ -1,7 +1,24 @@
 import {Environment, Vec3} from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
 
-export function SceneItem(object: string, lookup: any){
+/**
+ * 
+ * @param object Set the env object you want to change, use Chroma logs.
+ * @param lookup Lookup method, Regex, Contains, Exact, etc. 
+ * @param position Change env object to a vector 3 position. (Effect happens when disabled is set to false)
+ * @param scale Changes the env objects scale using a vector 3 position. (Effect happens when disabled is set to false)
+ * @param disabled true/false, self explanitory, will disable the environment 
+ */
+
+
+export function SceneObject(object: string, lookup: any, position: Vec3, scale: Vec3, disabled: boolean){
     const si = new Environment(object, lookup)
-    si.position = [999, 999, 999]
-    si.push();
+
+    if(disabled === true){
+        si.position = [0, -9999, 0]
+    }
+    else{
+        si.position = position
+        si.scale = scale
+    }
+    
 }
